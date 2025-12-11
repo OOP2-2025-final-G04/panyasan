30e7e7b16288f6b8aa0dda46ff7e3927a08fa355
Sota Tsukada <113979340+sotyan@users.noreply.github.com>
2025-12-12 07:02:36 +0900

    注文修正によるポイントの増減
    項目の変更ごとにポイントの増減をするのではなくて、再計算した方が早いです。

--- routes/order.py
+++ routes/order.py
@@ -37,38 +37,30 @@ def add():
 
 @order_bp.route('/edit/<int:order_id>', methods=['GET', 'POST'])
 def edit(order_id):
+    # 修正前のデータを取得
     order = Order.get_or_none(Order.id == order_id)
     user = User.get_by_id(order.user) # 購入者
     product = Product.get_by_id(order.product) # 製品
-    pre_order_count = order.count # 修正前の購入個数
-    price = product.price # 金額
-    point = user.point # 所持ポイント
-    this_order_got_point = int((pre_order_count * price) / 100) # この注文で獲得していたポイント
 
     if not order:
         return redirect(url_for('order.list'))
 
     if request.method == 'POST':
+        # 修正前のポイントをなくす
+        user.point -= int((product.price * int(order.count))/100) # 修正前に獲得していたポイントをマイナス
+        user.save()
+        # 修正後のデータを受け付ける
         order.user = request.form['user_id']
         order.product = request.form['product_id']
         order.count = request.form['order_count']
         order.save()
-        # ポイントの増減をする
-        if(product.id != order.product): # 購入した製品が変化した時
-            user.point -= this_order_got_point # この注文で獲得済みのポイントを、一旦なくす
-            # ポイントを追加する（修正後の個数で再計算）
-            total_price =(Product.get_by_id(order.product)).price * int(order.count)
-            get_point = int(total_price / 100)
-            user.point += get_point
-            user.save()
-        elif(pre_order_count != order.count): # 個数が変化した時
-            user.point -= this_order_got_point # この注文で獲得済みのポイントを、一旦なくす
-            # ポイントを追加する（修正後の個数で再計算）
-            total_price =price * int(order.count)
-            get_point = int(total_price / 100)
-            user.point += get_point
-            user.save()
-
+        # ポイントを追加する
+        user = User.get_by_id(order.user)
+        product = Product.get_by_id(order.product)
+        total_price = product.price * int(order.count)
+        get_point = int(total_price / 100)
+        user.point += get_point
+        user.save()
         return redirect(url_for('order.list'))
 
     users = User.select()
